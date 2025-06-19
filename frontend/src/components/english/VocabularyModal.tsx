0b09aafd4cea8332f5cb58c60a710cb2f3f262cb
minhvh <minhvh@sapo.vn>
2025-06-19 17:32:17 +0700

    fix: vocabulary modal

--- frontend/src/components/english/VocabularyModal.tsx
+++ frontend/src/components/english/VocabularyModal.tsx
@@ -1,41 +1,108 @@
-import { Button, Grid, TextInput, Textarea } from '@mantine/core'
+import {
+  Button,
+  ButtonGroup,
+  Grid,
+  Group,
+  TextInput,
+  Textarea,
+} from '@mantine/core'
 import { modals } from '@mantine/modals'
 import { useForm } from '@tanstack/react-form'
+import { IconPlus } from '@tabler/icons-react'
 import type { EnglishVocabularie } from '@/types/KnowledgeEnglish'
 
 export const VocabularyModal = () => {
   const form = useForm({
-    defaultValues: {} as EnglishVocabularie,
+    defaultValues: [{}] as Array<EnglishVocabularie>,
     onSubmit: ({ value }) => {
       handleSubmit(value)
     },
   })
 
-  const handleSubmit = (value: EnglishVocabularie) => {}
+  const { Field, state, insertFieldValue } = form
+
+  const handleSubmit = (value: Array<EnglishVocabularie>) => {}
 
   return (
     <>
-      <Grid>
-        <Grid.Col span={6}>
-          <TextInput
-            label="Your email"
-            placeholder="Your email"
-            data-autofocus
-          />
-        </Grid.Col>
+      {state.values.map((_, index) => {
+        return (
+          <Grid key={index}>
+            <Grid.Col span={6}>
+              <Field
+                name={`[${index}].word`}
+                children={(field) => (
+                  <TextInput
+                    label="Word"
+                    placeholder="Enter word"
+                    data-autofocus
+                    withAsterisk
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                )}
+              />
+            </Grid.Col>
 
-        <Grid.Col span={12}>
-          <Textarea
-            label="Your email"
-            placeholder="Your email"
-            data-autofocus
-          />
-        </Grid.Col>
-      </Grid>
+            <Grid.Col span={6}>
+              <Field
+                name={`[${index}].meaning`}
+                children={(field) => (
+                  <TextInput
+                    label="Meaning"
+                    placeholder="Meaning of word"
+                    withAsterisk
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                )}
+              />
+            </Grid.Col>
 
-      <Button fullWidth onClick={() => modals.closeAll()} mt="md">
-        Submit
-      </Button>
+            <Grid.Col span={12}>
+              <Field
+                name={`[${index}].definition`}
+                children={(field) => (
+                  <TextInput
+                    label="Definition"
+                    placeholder="Definition of word"
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                )}
+              />
+            </Grid.Col>
+
+            <Grid.Col span={12}>
+              <Field
+                name={`[${index}].example`}
+                children={(field) => (
+                  <Textarea
+                    label="Example"
+                    placeholder="Enter example"
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                )}
+              />
+            </Grid.Col>
+          </Grid>
+        )
+      })}
+
+      <Button.Group>
+        <Button
+          variant="subtle"
+          onClick={() => insertFieldValue('', 1)}
+          mt="md"
+        >
+          <IconPlus color="var(--mantine-color-red-text)" />
+          {/* Add more */}
+        </Button>
+        <Button fullWidth onClick={() => modals.closeAll()} mt="md">
+          Submit
+        </Button>
+      </Button.Group>
     </>
   )
 }
--- frontend/src/types/KnowledgeEnglish.ts
+++ frontend/src/types/KnowledgeEnglish.ts
@@ -1,1 +1,11 @@
-export interface EnglishVocabularie {}
+export interface EnglishVocabularie {
+  id: number
+  word: string
+  meaning: string
+  example?: string
+  pronunciation?: string
+  definition?: string
+  species?: string
+  word_relations?: Array<string>
+  tags?: Array<string>
+}
