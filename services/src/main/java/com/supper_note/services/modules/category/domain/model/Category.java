d8489c3eacdb4bcded28c6ec27695dc598eded82
minhvh <minhvuhoang1204@gmail.com>
2025-07-24 01:03:04 +0700

    fix: add base, object mapper, base exception

--- services/src/main/java/com/supper_note/services/modules/category/domain/model/Category.java
+++ services/src/main/java/com/supper_note/services/modules/category/domain/model/Category.java
@@ -1,5 +1,8 @@
 package com.supper_note.services.modules.category.domain.model;
 
+import lombok.Getter;
+
+@Getter
 public class Category {
     private Long userId;
     private String title;
--- services/src/main/java/com/supper_note/services/modules/category/domain/service/CategoryDomainService.java
+++ services/src/main/java/com/supper_note/services/modules/category/domain/service/CategoryDomainService.java
@@ -1,9 +1,12 @@
 package com.supper_note.services.modules.category.domain.service;
 
+import com.supper_note.services.modules.category.domain.model.Category;
 import org.springframework.stereotype.Service;
 
 @Service
 // thực hiện validate
 public class CategoryDomainService {
-
+    public boolean isValid(Category category) {
+        return category.getType() != null;
+    }
 }
